a94818fba29c251a11a8e1008b1789a9d5639c21
addictivepixels <addictivepixels@mac.com>
2022-02-16 12:11:57 -0500

    Update psWinUpgrader.ps1

--- psWinUpgrader.ps1
+++ psWinUpgrader.ps1
@@ -1,3 +1,5 @@
+#Original script by Warwick Burns
+#Added tidy-up bit to assist with some devices that had issues with the script or didn't cleanup previously.
 #tidy up old version - if there
 Remove-Item 'C:\_Windows_FU\' -Recurse -Force -WarningAction SilentlyContinue
 $dir = 'C:\_Windows_FU\packages'
